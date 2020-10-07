f55f5b197f6ebc9e0582b1e0b4704ea4bdc48dba
Tehjean Powell <tehjean.powell@gmail.com>
2020-10-07 18:03:05 -0500

    reconciled the mysql db config not reading env variables

--- src/config/mysql_db.js
+++ src/config/mysql_db.js
@@ -1,10 +1,14 @@
+if((process.env.NODE_ENV || 'dev') == 'dev') {
+    require("dotenv").config();
+}
+
 const mysql = require('mysql');
 
 const MYSQL_CONN = mysql.createPool({
-    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,//30,
-    supportBigNumbers: process.env.MYSQL_SUPPORT_BIG_NUMBERS,//true,
-    dateStrings: process.env.MYSQL_USE_DATE_STRINGS,// true,
-    multipleStatements: process.env.MYSQL_ALLOW_MULTIPLE_STATEMENTS,//true,
+    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+    supportBigNumbers: process.env.MYSQL_SUPPORT_BIG_NUMBERS,
+    dateStrings: process.env.MYSQL_USE_DATE_STRINGS,
+    multipleStatements: process.env.MYSQL_ALLOW_MULTIPLE_STATEMENTS,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
